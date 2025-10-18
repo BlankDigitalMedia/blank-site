0e774400e3a1f2d1c048a8a7aa86665fa8fcd780
Dave Blank <blankdigitalmediaco@gmail.com>
2025-10-18 14:00:39 -0400

    Crop Tenor GIFs horizontally by 20% to remove white padding

--- app/(post)/components/Tenor.tsx
+++ app/(post)/components/Tenor.tsx
@@ -10,12 +10,19 @@ interface TenorProps {
 }
 
 export default function Tenor({ postid, aspectRatio = "1.65625", width = "100%", title, displayTitle, searchLink }: TenorProps) {
-return (
-<>
-<div className="tenor-gif-embed" data-postid={postid} data-share-method="host" data-aspect-ratio={aspectRatio} data-width={width}>
-  <a href={`https://tenor.com/view/${title}-gif-${postid}`}>{displayTitle} GIF</a> from <a href={searchLink}>Wolf Of Wall Street GIFs</a>
-</div>
-  <script type="text/javascript" async src="https://tenor.com/embed.js"></script>
-  </>
+  return (
+    <div style={{ overflow: 'hidden', width: '100%' }}>
+      <div
+        className="tenor-gif-embed"
+        data-postid={postid}
+        data-share-method="host"
+        data-aspect-ratio={aspectRatio}
+        data-width="120%"
+        style={{ marginLeft: '-10%' }}
+      >
+        <a href={`https://tenor.com/view/${title}-gif-${postid}`}>{displayTitle} GIF</a> from <a href={searchLink}>Wolf Of Wall Street GIFs</a>
+      </div>
+      <script type="text/javascript" async src="https://tenor.com/embed.js"></script>
+    </div>
   )
 }
