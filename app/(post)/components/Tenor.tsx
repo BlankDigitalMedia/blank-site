f2413f695a1c7fe451e1b6bdd016f4cbe59b162d
Dave Blank <blankdigitalmediaco@gmail.com>
2025-10-18 14:03:45 -0400

    Crop confused GIF horizontally by 20% using clip-path to remove white padding without stretching

--- app/(post)/components/Tenor.tsx
+++ app/(post)/components/Tenor.tsx
@@ -1,7 +1,8 @@
 'use client'
 
 interface TenorProps {
-  postid: string
+  postid?: string
+  gifUrl?: string
   aspectRatio?: string
   width?: string
   title: string
@@ -9,7 +10,22 @@ interface TenorProps {
   searchLink: string
 }
 
-export default function Tenor({ postid, aspectRatio = "1.65625", width = "100%", title, displayTitle, searchLink }: TenorProps) {
+export default function Tenor({ postid, gifUrl, aspectRatio = "1.65625", width = "100%", title, displayTitle, searchLink }: TenorProps) {
+  if (gifUrl) {
+    return (
+      <div style={{ width: '100%', overflow: 'hidden' }}>
+        <img
+          src={gifUrl}
+          alt={displayTitle}
+          style={{ clipPath: 'inset(0 10% 0 10%)', width: '100%', height: 'auto' }}
+        />
+        <p style={{ textAlign: 'center', marginTop: '0.5rem' }}>
+          <a href={`https://tenor.com/view/${title}-gif-${postid || ''}`}>{displayTitle} GIF</a> from <a href={searchLink}>GIFs</a>
+        </p>
+      </div>
+    )
+  }
+
   return (
     <div style={{ overflow: 'hidden', width: '100%' }}>
       <div
@@ -17,8 +33,7 @@ export default function Tenor({ postid, aspectRatio = "1.65625", width = "100%",
         data-postid={postid}
         data-share-method="host"
         data-aspect-ratio={aspectRatio}
-        data-width="120%"
-        style={{ marginLeft: '-10%' }}
+        data-width={width}
       >
         <a href={`https://tenor.com/view/${title}-gif-${postid}`}>{displayTitle} GIF</a> from <a href={searchLink}>Wolf Of Wall Street GIFs</a>
       </div>
