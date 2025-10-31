4218e5157fa41ffd9e81d007f9921a4b80c09161
Dave Blank <blankdigitalmediaco@gmail.com>
2025-10-31 07:32:59 -0400

    Remove Murphie test prototype to fix build

--- tests/murphie/example.spec.ts
+++
@@ -1,7 +0,0 @@
-import { test, expect } from 'playwright/test';
-import { smartClick, smartFill } from '../../.murphie/helpers/autonomy';
-
-test('murphie example', async ({ page }) => {
-  await page.goto('https://playwright.dev');
-  await expect(page).toHaveTitle(/Playwright/);
-});
